5be7a2de6f323cb3b69c6e07082866f85687ebdc
Rahul Kulal <rahulkulala7@gmail.com>
2025-11-03 12:01:11 +0530

    Update AuthFilter.java

--- src/main/java/com/yourdomain/studyplatform/controller/AuthFilter.java
+++ src/main/java/com/yourdomain/studyplatform/controller/AuthFilter.java
@@ -1,12 +1,13 @@
 package com.yourdomain.studyplatform.controller;
 
 import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter; // ANNOTATION IS BACK
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import java.io.IOException;
 
-// NOTE: @WebFilter("/*") annotation has been REMOVED
+@WebFilter("/*") // ANNOTATION IS BACK
 public class AuthFilter implements Filter {
 
     @Override
@@ -15,25 +16,20 @@ public class AuthFilter implements Filter {
         
         HttpServletRequest req = (HttpServletRequest) request;
         HttpServletResponse res = (HttpServletResponse) response;
-        HttpSession session = req.getSession(false); // Do not create a session if one doesn't exist
+        HttpSession session = req.getSession(false);
         
         String path = req.getRequestURI().substring(req.getContextPath().length());
 
-        // Paths that do NOT require authentication
         boolean isPublicPath = path.equals("/login") || path.equals("/register") || path.startsWith("/css/");
-        
         boolean isLoggedIn = (session != null && session.getAttribute("userId") != null);
 
         if (isLoggedIn || isPublicPath) {
-            // User is logged in OR is accessing a public page. Allow access.
             chain.doFilter(request, response);
         } else {
-            // User is NOT logged in and is accessing a protected page. Redirect to login.
             res.sendRedirect("login"); 
         }
     }
 
-    // You can leave init() and destroy() empty
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {}
 
